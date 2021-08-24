86d3ac4def574fcdf5a376d9efce330b0a09e763
andre ferrer <andrerferrer@hotmail.com>
2021-08-24 06:42:37 -0300

    Implement the scraping

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def home
+
+    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.854.0 Safari/535.2"
+    
+    url = "https://eshop-prices.com/games/1566-super-smash-bros-ultimate"
+    
+    doc = Nokogiri::HTML(open(url, 'User-Agent' => user_agent), nil, "UTF-8")
+    @country_prices = doc.search('.prices-table .pointer').map(&:to_html)
+    
   end
 end
+
